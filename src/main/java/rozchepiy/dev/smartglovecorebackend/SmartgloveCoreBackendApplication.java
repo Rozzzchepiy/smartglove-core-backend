eef383c7c9afba0b11d50bd1b2a8ee8513d967c9
Rozzzchepiy <rozchepijyura@gmail.com>
2026-04-18 10:18:08 +0300

    chore: initialize spring boot project with docker infrastructure

---
+++ src/main/java/rozchepiy/dev/smartglovecorebackend/SmartgloveCoreBackendApplication.java
@@ -0,0 +1,13 @@
+package rozchepiy.dev.smartglovecorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgloveCoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartgloveCoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rozchepiy/dev/smartglovecorebackend/SmartgloveCoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rozchepiy.dev.smartglovecorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgloveCoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
