ca5729165e2c3533dd73401dc05a3108f04d819d
Rozzzchepiy <rozchepijyura@gmail.com>
2026-04-18 23:48:26 +0300

    feat: integrate MinIO client and auto-create buckets on startup

---
+++ src/main/java/rozchepiy/dev/smartglovecorebackend/service/MinioService.java
@@ -0,0 +1,35 @@
+package rozchepiy.dev.smartglovecorebackend.service;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket-name}")
+    private String bucketName;
+
+    @PostConstruct
+    public void init() {
+        try {
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder()
+                    .bucket(bucketName)
+                    .build());
+
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder()
+                        .bucket(bucketName)
+                        .build());
+            }
+        } catch (Exception e) {
+        }
+    }
+}
